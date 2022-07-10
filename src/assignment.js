e288f7d6c192d55c8fac784b474bce5846f4a596
Charles Ilo <71842782+charlesik@users.noreply.github.com>
2022-07-10 13:35:50 +0000

    challenge 1 and 3

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   let sum = 0;
+  for (let index = 0; index < arrayOfNumbers.length; index++) {
+      sum += arrayOfNumbers[index]
+    }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -47,9 +51,14 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+  let farenheitTemp = []
+  for (const index in arrayOfNumbers) {
+    let farenheit = (arrayOfNumbers[index] * 9/5) + 32
+    farenheitTemp.push(Math.trunc(farenheit))
+    }
+  return farenheitTemp
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
